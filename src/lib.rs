642bdd67c638eebbd266b80e88fc3b37b3b909b3
Chris <ignatz@gmail.com>
2015-09-10 21:46:27 -0400

    Fixed test to use military time.

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ use chrono::*;
 
 #[test]
 fn hours_served__rounds_up() {
-    assert_eq!(hours_served(NaiveTime::from_hms(6, 30, 0), NaiveTime::from_hms(7, 29, 0)), Ok(1));
+    assert_eq!(hours_served(NaiveTime::from_hms(18, 30, 0), NaiveTime::from_hms(19, 29, 0)), Ok(1));
 }
