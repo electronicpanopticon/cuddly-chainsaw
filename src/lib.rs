414a0d0606fee230f16b2bd01d23d918c0aaf63a
Chris <ignatz@gmail.com>
2015-09-12 15:04:05 -0400

    hours_served_rounds_up() test initial pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
 extern crate chrono;
 
+use std::io;
+
 use chrono::*;
+
+//// Returns Rust [Result](http://rustbyexample.com/std/result.html)
+//// representing the total hours that the baby sitter will get credit for.
+fn hours_served(_from: NaiveTime, _to: NaiveTime) -> Result<i32, io::Error> {
+    let r: i32 = 1;
+    Ok(r)
+}
+
 
 #[test]
 fn hours_served__rounds_up() {
-    assert_eq!(hours_served(NaiveTime::from_hms(18, 30, 0), NaiveTime::from_hms(19, 29, 0)), Ok(1));
+
+    let x: Result<i32, io::Error> = hours_served(NaiveTime::from_hms(18, 30, 0), NaiveTime::from_hms(19, 29, 0));
+    assert_eq!(x.unwrap(), 1);
 }
