831c0c176938a3b9387c1255e301a400e1e6ec65
Chris <ignatz@gmail.com>
2015-09-26 11:58:17 -0400

    Got range to work for hours served test. Now if fails

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate chrono;
 
+// use duration::Duration;
 use std::io;
 
 use chrono::*;
@@ -7,6 +8,10 @@ use chrono::*;
 //// Returns Rust [Result](http://rustbyexample.com/std/result.html)
 //// representing the total hours that the baby sitter will get credit for.
 fn hours_served(_from: NaiveTime, _to: NaiveTime) -> Result<i32, io::Error> {
+    
+    let diff = _from - _to;
+    println!("Response: {}", diff.num_minutes());
+    
     let r: i32 = 1;
     Ok(r)
 }
@@ -15,6 +20,17 @@ fn hours_served(_from: NaiveTime, _to: NaiveTime) -> Result<i32, io::Error> {
 #[test]
 fn hours_served__rounds_up() {
 
-    let x: Result<i32, io::Error> = hours_served(NaiveTime::from_hms(18, 30, 0), NaiveTime::from_hms(19, 29, 0));
-    assert_eq!(x.unwrap(), 1);
+    let start_hour:i32 = 18;
+
+    let start =  NaiveTime::from_hms(start_hour as u32, 30, 0);
+
+    for x in 19..23 {
+        
+        let end = NaiveTime::from_hms(x as u32, 29, 0);
+
+        let r: Result<i32, io::Error> = hours_served(start, end);
+        let diff = start_hour - x;
+        assert_eq!(r.unwrap(), diff as i32);  
+
+    }
 }
